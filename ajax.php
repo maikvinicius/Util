63818edca38ba1e0951c56bf349ab9f32315bf60
maikvinicius <maik_ga-eu@hotmail.com>
2016-12-06 14:25:26 -0200

    Botão com Ajax sem Segurança! :zap:

---
+++ ajax.php
@@ -0,0 +1,46 @@
+
+<input type="text" name="teste" id="teste">
+
+<script type="text/javascript">
+
+  function salvar(id_foto){
+
+    var teste = $("#teste").val();
+
+    var request = $.ajax({//post
+      url: "index.php?route=salvar",
+      type: "POST",
+      data: {"teste": teste},
+      dataType: "json"
+    });
+
+    request.done(function(data) {//verifica o resultado
+      console.log(data);
+      alert('Atualizada com sucesso!');
+    });
+
+    request.fail(function(jqXHR, textStatus) {//caso haja erro
+      alert( "Falha: " + textStatus );
+    });
+
+  }
+  </script>
+
+  <?php
+
+  public function salvar(){
+
+		$data = array();
+
+		$data['teste'] = $this->request->post['teste'];
+
+		$json = array(
+			'sucesso' => 'ok'
+		);
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+
+	}
+
+  ?>
